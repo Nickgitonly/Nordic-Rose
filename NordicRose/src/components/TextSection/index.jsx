03832b3cf0809429e28e98565db2ab59d63dd25f
Nastya <anastasis.stelmakh@gmail.com>
2024-11-05 14:47:33 +0200

    xxx

--- NordicRose/src/components/TextSection/index.jsx
+++ NordicRose/src/components/TextSection/index.jsx
@@ -9,7 +9,7 @@ function TextSection() {
                 <p>This lovely web is full of everything which is created I don't know what in mind, considering that sometimes totally bonkers stuff might be highly relevant if it has the right kind of thinking behind it.</p>
             </div>
             <div className="image-caption-section">
-                <img src="images/Rectangle 8.png" alt="" />
+                <img src="images/Rectangle 88.png" alt="" />
                 <p>Image caption centered this way and I’ll make this a bit longer to indicate the amount of line-height.</p>
             </div>
         </div>

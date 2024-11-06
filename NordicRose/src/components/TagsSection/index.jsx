620d81657d326fe662d28550ba7244c0058192a0
Nastya <anastasis.stelmakh@gmail.com>
2024-11-06 14:39:35 +0200

    Tegs/Section/TagsLine/Add 2 components/ImagesBlock/ImagesBlock_Line

--- NordicRose/src/components/TagsSection/index.jsx
+++ NordicRose/src/components/TagsSection/index.jsx
@@ -8,9 +8,6 @@ function TagsSection() {
             Tags:<span className="underline">product design</span>,<span className="underline">culture</span>.
             </p>
             </div>
-            <div className="line6">
-                    <img src="images/Line 11.png" alt="" />
-            </div>
         </div>
     );
 }

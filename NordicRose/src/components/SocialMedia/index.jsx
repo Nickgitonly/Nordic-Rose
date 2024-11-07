b24f371bd577f10b6a8a1cf6322266a48421954e
Nastya <anastasis.stelmakh@gmail.com>
2024-11-07 10:14:49 +0200

    bvbvbv

--- NordicRose/src/components/SocialMedia/index.jsx
+++ NordicRose/src/components/SocialMedia/index.jsx
@@ -3,9 +3,9 @@ import './style.css';
 function SocailMedia() {
     return (
         <div className="social-icons">
-                <button className="btn"><img src="images/facebook.png" alt="" /><span className='btn-txt'>Share on Facebook</span></button>
-                <button className="btn"><img src="images/twitter.png" alt="" /><span className='btn-txt'>Share on Twitter</span></button>
-                <button className="btn"><img src="images/whatsapp.png" alt="" /><span className='btn-txt'>Share on WhatsApp</span></button>
+                <button className="btn"><img src="images/facebook.png" alt="" /></button>
+                <button className="btn"><img src="images/twitter.png" alt="" /></button>
+                <button className="btn"><img src="images/whatsapp.png" alt="" /></button>
         </div>
     );
 }

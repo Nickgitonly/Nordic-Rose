adefae36a70d57c09037365f232897843dbc026c
leracheburek <lerayokovenko098@gmail.com>
2024-11-10 18:13:07 +0200

    uyfyuf8uly

--- NordicRose/src/components/TextAboutSite/index.jsx
+++ NordicRose/src/components/TextAboutSite/index.jsx
@@ -2,7 +2,7 @@ import './style.css';
 
 function TextAboutSite() {
     return (
-        <div className="text-section">
+        <div className="text2">
                 This lovely web is full of everything which is created I don't know what in mind, considering that sometimes totally bonkers stuff might be highly relevant if it has the right kind of thinking behind it.
                 <br /><br />
                 Luckily, in the middle of all that, there are some pockets of content that offer delightfully valuable contrast to cursory wisdom in some of the established channels.

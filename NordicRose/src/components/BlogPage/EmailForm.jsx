810451a01bad5dcd1bddaa1c8670fc54f2880e40
Vika <viktoriia.zozulia@nure.ua>
2024-11-10 19:08:12 +0200

    Fixed components BlogPage and EmailForm

--- NordicRose/src/components/BlogPage/EmailForm.jsx
+++ NordicRose/src/components/BlogPage/EmailForm.jsx
@@ -1,23 +1,26 @@
 export default function EmailForm() {
     return (
-        <div className="font-sans w-[90%] max-w-[500px] mx-auto my-10">
-            <div className="h-2 bg-black w-full my-4"></div>
-            <div className="bg-white border border-black p-6 box-border text-center">
-                <h2 className="font-serif text-2xl mb-4">Sign up for the newsletter</h2>
-                <p className="text-sm leading-relaxed mb-5">
+        <div className="w-[90%] max-[400px]:w-[100%] max-w-[500px] mx-auto my-10">
+            <div className="h-2 bg-black w-full"></div>
+            <div className="bg-white font-['San_Francisco'] border max-[400px]:border-x-0 border-black p-6 text-center">
+                <h2 className="font-serif text-2xl mb-2 leading-none font-bold">Sign up for the newsletter</h2>
+                <p className="text-sm font-medium font-['San_Francisco'] mb-5">
                     If you want relevant updates occasionally, sign up for the private
                     newsletter. Your email is never shared.
                 </p>
-                <form className="flex max-[400px]:flex-col gap-2 justify-center items-center mt-4">
-                    <input
-                        type="email"
-                        placeholder="Enter your email"
-                        className="flex-1 min-w-[70px] p-2.5 border border-gray-300 text-sm"
-                    />
-                    <button className="px-5 py-2.5 bg-black text-white border-none text-sm cursor-pointer font-bold uppercase whitespace-nowrap">
-                        Sign Up
-                    </button>
-                </form>
+
+                <div className="flex justify-center items-center">
+                    <form className="flex w-[100%] justify-center items-center mb-4">
+                        <input
+                            type="email"
+                            placeholder="Enter your email..."
+                            className="flex-1 font-['San_Francisco'] min-w-[70px] px-2 py-2.5 border border-black text-sm text-black placeholder-black"
+                        />
+                        <button className="px-5 py-2.5 font-semibold bg-black text-white border border-black text-sm cursor-pointer font-medium uppercase whitespace-nowrap tracking-widest">
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     );
